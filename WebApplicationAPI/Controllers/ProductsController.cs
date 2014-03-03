4b6eed52b74a488187e141f87576ac56bead2a59
James Logan <logan4dog@gmail.com>
2014-03-03 09:49:45 -0500

    Web API Example

---
+++ WebApplicationAPI/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using WebApplicationAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplicationAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[] {
+            new Product {Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1.25M},
+            new Product {Id = 2, Name = "Yo Yo", Category = "Toys", Price = 3.25M},
+            new Product {Id = 3, Name = "Hammer", Category = "Hardware", Price = 15.25M}
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int Id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == Id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+    }
+}
